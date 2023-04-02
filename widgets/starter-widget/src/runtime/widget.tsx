9a5ffd577397b5ff7d09237b231f016a5ac8b905
Rachel Cepeda <rachelcepeda9@gmail.com>
2023-04-01 23:51:17 -0500

    Add files via upload

---
+++ widgets/starter-widget/src/runtime/widget.tsx
@@ -0,0 +1,9 @@
+import { React, AllWidgetProps } from 'jimu-core'
+
+const Widget = (props: AllWidgetProps<any>) => {
+  return (
+    <div className="widget-starter jimu-widget">This is your starter widget!</div>
+  )
+}
+
+export default Widget
